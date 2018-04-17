31fd71bb449a6239c7234d5c75916575e50d4fc4
hirokisan <expsharetoworld@gmail.com>
2018-04-17 23:00:01 +0900

    variable.php

---
+++ variable.php
@@ -0,0 +1,18 @@
+<?php
+
+
+$array = [
+    0 => '1',
+    1 => '2',
+];
+
+foreach($array as $key => $value){
+    if($key == 0)
+    {
+        $aaa = 'okay';
+    }
+}
+?>
+<?php
+
+var_dump($aaa);
